73ad6e9e9d05a4e7fb96f1c93c6fdb2a0ab64af1
IBOUU03 <118839500+IBOUU03@users.noreply.github.com>
2022-12-07 01:37:59 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿int number;
+do
+{
+    Console.WriteLine("Qiyməti qeyd edin");
+    number = Convert.ToInt32(Console.ReadLine());
+    
+} while (number < 0 || number > 100);
+
+if (number <= 100 && number > 90)
+{
+    Console.WriteLine("A");
+}
+else if (number <= 90 && number > 80)
+{
+    Console.WriteLine("B");
+}
+else if (number <= 80 && number > 70)
+{
+    Console.WriteLine("C");
+}
+else if (number <= 70 && number > 60)
+{
+    Console.WriteLine("D");
+}
+else if (number <= 60 && number > 50)
+{
+    Console.WriteLine("E");
+}
+else
+{
+    Console.WriteLine("Kəsildiniz");
+}
---
+++ Program1.cs
@@ -0,0 +1,15 @@
+﻿int number;
+do
+{
+    Console.WriteLine("Ədəd daxil edin");
+    number = Convert.ToInt32(Console.ReadLine());
+} while (number <= 0);
+
+if (number % 3 == 0 && number % 7 ==0)
+{
+    Console.WriteLine("Ədəd 7 və 3ə bölünür");
+}
+else
+{
+    Console.WriteLine("Ədəd 7 və 3ə bölünmür");
+}
